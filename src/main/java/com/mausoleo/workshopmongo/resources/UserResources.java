fdfa1575a758b52e7c6b14be6760f88a5d6c6df2
mausoleo <leomelo35@hotmail.com>
2025-03-15 20:11:56 -0300

    Inserção de usuário com POST

--- src/main/java/com/mausoleo/workshopmongo/resources/UserResources.java
+++ src/main/java/com/mausoleo/workshopmongo/resources/UserResources.java
@@ -1,12 +1,17 @@
 package com.mausoleo.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mausoleo.workshopmongo.domain.User;
 import com.mausoleo.workshopmongo.dto.UserDTO;
@@ -24,5 +29,19 @@ public class UserResources {
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).toList();
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(user));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/mausoleo/workshopmongo/services/UserService.java
+++ src/main/java/com/mausoleo/workshopmongo/services/UserService.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mausoleo.workshopmongo.domain.User;
+import com.mausoleo.workshopmongo.dto.UserDTO;
 import com.mausoleo.workshopmongo.repository.UserRepository;
+import com.mausoleo.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,20 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		return repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(
+				objDto.getId()
+				,objDto.getName()
+				,objDto.getEmail());
 	}
 }
