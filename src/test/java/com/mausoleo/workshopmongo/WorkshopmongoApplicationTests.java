1bbd605eaadbd2b7c980fc4bb7785d7cdacbb829
mausoleo <leomelo35@hotmail.com>
2025-03-15 11:55:15 -0300

    Projeto Criado

---
+++ src/test/java/com/mausoleo/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mausoleo.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
